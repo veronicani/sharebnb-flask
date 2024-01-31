508d43612586addcb9717e3042dc4633945010b1
Veronica Ni <vni1294@gmail.com>
2024-01-30 19:22:51 -0500

    Added models for User and Image

--- models.py
+++ models.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Properties(db.Model):
+
+class Property(db.Model):
     """Current properties in the system. """
 
     __tablename__ = 'properties'
@@ -28,7 +29,6 @@ class Properties(db.Model):
     address = db.Column(
         db.String(50),
         nullable=False,
-        default="",
     )
 
     price = db.Column(
@@ -48,3 +48,66 @@ class Properties(db.Model):
         default=False,
     )
 
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id', ondelete='CASCADE'),
+        nullable=False,
+    )
+
+    images = db.relationship('Image', backref="property")
+    # backref'ed in User Model
+    # user = db.relationship('Property', backref="properties")
+
+
+class User(db.Model):
+    """Current users in the system. """
+
+    __tablename__ = 'users'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    username = db.Column(
+        db.String(30),
+        unique=True,
+        nullable=False,
+    )
+
+    first_name = db.Column(
+        db.String(30),
+        nullable=False,
+    )
+
+    last_name = db.Column(
+        db.String(50),
+        nullable=False,
+    )
+
+    email = db.Column(
+        db.String(50),
+        nullable=False,
+    )
+
+    properties = db.relationship('Property', backref="user")
+
+
+class Image(db.Model):
+    """Current images for properties in the system."""
+
+    __tablename__ = 'images'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    properties_id = db.Column(
+        db.Integer,
+        db.ForeignKey('properties.id', ondelete='CASCADE'),
+        nullable=False,
+    )
+
+    # backref'd in Property Model
+    # property = db.relationship('Property', backref="images")
