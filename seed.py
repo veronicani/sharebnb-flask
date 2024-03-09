7798ba1abcffd83135fe61b1d40fc68d9d214bc7
Veronica Ni <vni1294@gmail.com>
2024-03-09 15:33:41 -0500

    Added more seed data

--- seed.py
+++ seed.py
@@ -28,11 +28,10 @@ p1 = Property(
     name="Sunny Oasis with Private Pool",
     description=(
         "Escape the summer heat and dive into your own private"
-        + " paradise! Our spacious backyard oasis features a"
-        + " sparkling pool, lounge chairs, and a shaded patio perfect"
+        + " paradise in our spacious backyard. Perfect"
         + " for relaxing with friends and family."
         ),
-    price=10,
+    price=56,
     address="123 Main Street, Sunnydale Suburb, CA, 98765",
     backyard=False,
     pool=True,
@@ -42,9 +41,9 @@ p1 = Property(
 p2 = Property(
     name="Cozy Backyard Retreat",
     description=(
-        "Discover serenity in our charming backyard retreat! Nestled"
-        + " among towering trees and vibrant foliage, our cozy outdoor space"
-        + " offers a peaceful escape from the hustle and bustle of city life."
+        "Discover serenity in our charming backyard retreat!"
+        + " our cozy outdoor space offers a peaceful"
+        + " escape from the hustle and bustle of city life."
     ),
     price=100,
     address="456 Elm Avenue, Oakwood Heights Suburb, NY, 54321",
@@ -57,17 +56,43 @@ p3 = Property(
     name="Ultimate Outdoor Haven: Backyard & Pool Combo",
     description=(
         "Experience the best of both worlds in our ultimate outdoor haven!"
-        + " Our spacious backyard oasis boasts a pristine pool, lush greenery,"
-        + " and ample seating areas for lounging and entertaining."
+        + " We boast ample seating areas for lounging and entertaining."
     ),
-    price=1000,
+    price=350,
     address="789 Oak Street, Maplewood Suburb, NJ, 67890",
     backyard=True,
     pool=True,
     user_id=2,
 )
 
-db.session.add_all([p1, p2, p3])
+p4 = Property(
+    name="Tranquil Garden Hideaway",
+    description=(
+        "Step into a serene garden hideaway and leave the stresses"
+        + " of city life behind!"
+    ),
+    price=70,
+    address="345 Willow Lane, Willow Creek Suburb, OR, 45678",
+    backyard=False,
+    pool=True,
+    user_id=2,
+)
+
+p5 = Property(
+    name="Sun-Kissed Pool Retreat",
+    description=(
+        "Indulge in the ultimate relaxation at our sun-kissed pool retreat!" 
+        + " Immerse yourself in crystal-clear waters and enjoy our spacious"
+        + " deck."
+    ),
+    price=70,
+    address="234 Palm Boulevard, Palm Grove Suburb, FL, 34567",
+    backyard=False,
+    pool=True,
+    user_id=2,
+)
+
+db.session.add_all([p1, p2, p3, p4, p5])
 db.session.commit()
 
 # Images ##############################
@@ -95,13 +120,36 @@ i2 = Image(
 
 i3 = Image(
     property_id=3,
-    aws_key="789",
+    aws_key="333",
     url=(
-        "https://st.hzcdn.com/simgs/pictures/pools/"
-        + "natural-private-residence-pool-environments-inc-img"
-        + "~66e1401200abf6be_14-3383-1-325dc2d.jpg"
+        "https://images.unsplash.com/photo-1576013551627"
+        + "-0cc20b96c2a7?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid"
+        + "=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     ),
 )
 
-db.session.add_all([i1, i2, i3])
+i4 = Image(
+    property_id=4,
+    aws_key="444",
+    url=(
+        "https://images.unsplash.com/photo-1611282712338-63a58e27980a?q="
+        + "80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid="
+        + "M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    ),
+
+)
+
+i5 = Image(
+    property_id=5,
+    aws_key="555",
+    url=(
+        "https://images.unsplash.com/photo-1576013551627"
+        + "-0cc20b96c2a7?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid"
+        + "=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    ),
+
+)
+
+
+db.session.add_all([i1, i2, i3, i4, i5])
 db.session.commit()
