6660a868b0da4c6ff385febaf3d9c76736af53da
Veronica Ni <vni1294@gmail.com>
2024-04-01 12:23:40 -0400

    Updated i5 to be diff from i3

--- seed.py
+++ seed.py
@@ -143,8 +143,8 @@ i5 = Image(
     property_id=5,
     aws_key="555",
     url=(
-        "https://images.unsplash.com/photo-1576013551627"
-        + "-0cc20b96c2a7?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid"
+        "https://images.unsplash.com/photo-1544984243-ec57ea16fe25?q=80&w=3174&"
+        + "auto=format&fit=crop&ixlib=rb-4.0.3&ixid"
         + "=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     ),
 
